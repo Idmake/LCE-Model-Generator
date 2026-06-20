b2f44b2298091d2f5b17e47e1e70f0acad2cba2a
stefansdog <stefan.landwehr1000@gmail.com>
2026-06-20 17:51:23 +0200

    Update old convert to xyz function

--- main.py
+++ main.py
@@ -69,9 +69,9 @@ def convert_to_scale(from_, to_):
 
 def convert_to_xyz(from_, whd):
     # Blockbench and LCE use different coordinate systems so we have to convert the positions
-    x =     from_[0] - whd[0] / 2
-    y =     from_[1] - whd[1] / 2
-    z =     from_[2] - whd[2] / 2
+    x =     from_[0] - (16 / 2)
+    y =    -from_[1] - whd[1] - (16 / 2)
+    z =     from_[2] - (16 / 2)
 
     return [x, y, z]
 
